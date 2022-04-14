73c2655d0f64cbd4ce8793d9819215ea337b8156
Philippe  Stemberger <pstember@gmail.com>
2022-04-14 14:15:43 +0100

    removed gogs dependency has no longer exists

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ import (
 	"fmt"
 	"go-goof/hello"
     "go-goof/handlers"
-    "github.com/gogs/gogs/pkg/tool"
     "github.com/gin-gonic/gin"
 )
 func main() {
-		fmt.Println(tool.MD5(hello.Hello()))
+		fmt.Println(hello.Hello())
         r := gin.Default()
         r.GET("/ping", handlers.Ping)
         r.Run() // listen and serve on 0.0.0.0:8080
